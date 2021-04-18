aab2cc07baafe46df2b2b50b101a76d71d064dbe
hanglomo <shhlomo@Outlook.com>
2021-04-18 01:35:27 -0800

    Add files via upload
    孙邵宬第一节课课堂源码

---
+++ class1-text.py
@@ -0,0 +1,13 @@
+    print("Hello World!")#输出语句
+    print("   *   ")
+    print("  ***")
+    print(" *****")
+    print("*******")
+    print("我叫孙劭宬")
+  
+    #计算海绵宝宝与派大星年龄和
+    a= int(input("请输入海绵宝宝年龄："))#输入语句
+    b=int(input("请输入派大星年龄："))
+    f=a+b
+    print("他们的年龄和是：",f)
+

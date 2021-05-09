f0952d7c6ffedf86036ad7a11d21705c0fa52f04
hanglomo <shhlomo@Outlook.com>
2021-05-09 18:19:31 +0800

    Create class2-test1.py
    孙邵宬第二节课课堂源码

---
+++ class2-test1.py
@@ -0,0 +1,27 @@
+name="孙劭宬"
+print("孙劭宬"*100)
+
+a=int(input("请输入你有多少钱："))
+b=int(input("请输入苹果单价："))
+c=a/b
+print("你能买{}斤苹果",c)
+print(a%b)
+#价格自动计算器
+a=6.8
+b=int(input("请输入鸡蛋个数："))
+c=a*b
+print("总价为:",c)
+
+import turtle as t
+t.shape('turtle')
+t.pensize(15)
+t.color('yellow')
+t.fd(50)
+t.pensize(5)
+t.color('red')
+t.fd(50)
+t.pensize(15)
+t.color('yellow')
+t.fd(50)
+
+

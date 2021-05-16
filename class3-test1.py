f49f2838503c5d44054ecd50c39bbcd8e2a23682
hanglomo <shhlomo@Outlook.com>
2021-05-16 18:11:42 +0800

    Create class3-test1.py
    孙邵宬第三节课课堂源码

---
+++ class3-test1.py
@@ -0,0 +1,12 @@
+sports_list=["basketball","football","badminton"]  
+print(sports_list)   
+list1=["name",23,"age"]       
+list2=[3.14,"hobby","爱好"]   
+list1=[]       
+list2=[]   
+list3=[list1,list2]    
+print(list3)   
+"age" in list1 
+23 in list1  
+list1=[1,2,3,4,5,6,7,8,9,0,3,2,6,5,4,7,86,4,3,534,56,6,5,776]
+print(list1[::-1])
